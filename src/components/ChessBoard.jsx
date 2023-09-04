d0a97d5b2e23fb796960a2442fcd92a40d5f065e
omarlamin01 <omarlamin272@gmail.com>
2023-09-04 22:31:41 +0100

    Update ChessBoard.jsx

---
+++ src/components/ChessBoard.jsx
@@ -0,0 +1,31 @@
+
+function ChessBoard() {
+    const rows = 8;
+    const cols = 8;
+
+    const chessboard = [];
+
+    for (let row = 0; row < rows; row++) {
+        for (let col = 0; col < cols; col++) {
+            const isWhiteSquare = (row + col) % 2 === 0;
+            const squareColor = isWhiteSquare ? "bg-white" : "bg-black";
+
+            chessboard.push(
+                <div
+                    key={`${row}-${col}`}
+                    className={`w-12 h-12 flex items-center justify-center ${squareColor}`}
+                >
+                    {/* You can add chess pieces or other content here */}
+                </div>
+            );
+        }
+    }
+
+    return (
+        <div className="grid grid-cols-8 gap-0">
+            {chessboard}
+        </div>
+    );
+}
+
+export default ChessBoard;
